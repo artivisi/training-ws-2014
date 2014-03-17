f95c2096d82ecfb0f254914fecacaad164ee6768
Endy Muhardin <endy.muhardin@gmail.com>
2014-03-17 15:01:22 +0700

    WS Client hasil generate oleh JAX-WS

---
+++ aplikasi-absen-ws-client/src/main/java/com/muhardin/endy/training/ws/aplikasi/absen/ws/client/Main.java
@@ -0,0 +1,7 @@
+package com.muhardin.endy.training.ws.aplikasi.absen.ws.client;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}

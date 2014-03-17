975c217014f7b4dab1c14ecf3fd46bd76e576448
Endy Muhardin <endy.muhardin@gmail.com>
2014-03-17 15:23:16 +0700

    memanggil SOAP method dari Java

--- aplikasi-absen-ws-client/src/main/java/com/muhardin/endy/training/ws/aplikasi/absen/ws/client/Main.java
+++ aplikasi-absen-ws-client/src/main/java/com/muhardin/endy/training/ws/aplikasi/absen/ws/client/Main.java
@@ -1,15 +1,17 @@
 package com.muhardin.endy.training.ws.aplikasi.absen.ws.client;
 
+import java.util.List;
+
 public class Main {
     private AbsenWS_Service service = new AbsenWS_Service();
+    private AbsenWS port = service.getAbsenWSPort();
     
     public void tambah(){
-        try { // Call Web Service Operation
-            AbsenWS port = service.getAbsenWSPort();
-            // TODO initialize WS operation arguments here
+        try { 
+            
             Integer num1 = 5;
             Integer num2 = 6;
-            // TODO process result here
+
             Integer result = port.tambah(num1, num2);
             System.out.println("Result = "+result);
         } catch (Exception ex) {
@@ -17,10 +19,24 @@ public class Main {
         }
     }
     
+    public void cariKaryawanByNip(){
+        Karyawan k = port.cariKaryawanByNip(123);
+        System.out.println("NIP : "+k.getNip());
+        System.out.println("Nama : "+k.getNama());
+        System.out.println("Email : "+k.getEmail());
+        System.out.println("Tanggal Lahir : "+k.getTanggalLahir());
+        
+        List<String> daftarTelp = k.getTelp();
+        for (String telp : daftarTelp) {
+            System.out.println("Telp : " +telp);
+        }
+    }
+    
     public static void main(String[] args) {
         
         Main m = new Main();
         m.tambah();
+        m.cariKaryawanByNip();
 
     }
 }

c93c7a60c3675b1c597e8b85b931ba5352bb916d
Endy Muhardin <endy.muhardin@gmail.com>
2014-03-17 15:19:20 +0700

    menggunakan method dari web services

--- aplikasi-absen-ws-client/src/main/java/com/muhardin/endy/training/ws/aplikasi/absen/ws/client/Main.java
+++ aplikasi-absen-ws-client/src/main/java/com/muhardin/endy/training/ws/aplikasi/absen/ws/client/Main.java
@@ -1,7 +1,26 @@
 package com.muhardin.endy.training.ws.aplikasi.absen.ws.client;
 
 public class Main {
+    private AbsenWS_Service service = new AbsenWS_Service();
+    
+    public void tambah(){
+        try { // Call Web Service Operation
+            AbsenWS port = service.getAbsenWSPort();
+            // TODO initialize WS operation arguments here
+            Integer num1 = 5;
+            Integer num2 = 6;
+            // TODO process result here
+            Integer result = port.tambah(num1, num2);
+            System.out.println("Result = "+result);
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+    }
+    
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        
+        Main m = new Main();
+        m.tambah();
+
     }
 }

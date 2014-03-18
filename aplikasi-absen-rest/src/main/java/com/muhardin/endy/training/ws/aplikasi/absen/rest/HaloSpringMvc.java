381b7a112f026acab9a8ff1025a47608a5c4a031
Endy Muhardin <endy.muhardin@gmail.com>
2014-03-18 15:41:48 +0700

    implementasi penjumlahan dengan Spring MVC

--- aplikasi-absen-rest/src/main/java/com/muhardin/endy/training/ws/aplikasi/absen/rest/HaloSpringMvc.java
+++ aplikasi-absen-rest/src/main/java/com/muhardin/endy/training/ws/aplikasi/absen/rest/HaloSpringMvc.java
@@ -3,6 +3,7 @@ package com.muhardin.endy.training.ws.aplikasi.absen.rest;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class HaloSpringMvc {
@@ -12,5 +13,13 @@ public class HaloSpringMvc {
         ModelMap mm = new ModelMap();
         mm.addAttribute("halo", "Hallo Spring MVC");
         return mm;
+    }
+    
+    @RequestMapping("/hitung")
+    public ModelMap hitung(@RequestParam Integer x, @RequestParam Integer y){
+        return new ModelMap()
+                .addAttribute("x", x)
+                .addAttribute("y", y)
+                .addAttribute("hasil", (x + y));
     }
 }
---
+++ aplikasi-absen-rest/src/main/webapp/WEB-INF/jsp/hitung.jsp
@@ -0,0 +1,17 @@
+<%-- 
+    Document   : halo
+    Created on : Mar 18, 2014, 3:16:21 PM
+    Author     : endy
+--%>
+
+<%@page contentType="text/html" pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title>Hasil Penjumlahan</title>
+    </head>
+    <body>
+        <h1>${x} + ${y} = ${hasil}</h1>
+    </body>
+</html>

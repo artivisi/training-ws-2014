991f3fefc8291207a39a2dede409c4f8c657a96a
Endy Muhardin <endy.muhardin@gmail.com>
2014-03-24 11:27:58 +0700

    RESTful method untuk mengembalikan data dalam format XML atau JSON

---
+++ aplikasi-absen-rest/src/main/java/com/muhardin/endy/training/ws/aplikasi/absen/rest/AbsenRestController.java
@@ -0,0 +1,21 @@
+package com.muhardin.endy.training.ws.aplikasi.absen.rest;
+
+import com.muhardin.endy.training.ws.aplikasi.absen.Karyawan;
+import com.muhardin.endy.training.ws.aplikasi.absen.service.AbsenService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class AbsenRestController {
+    @Autowired
+    private AbsenService absenService;
+    
+    @RequestMapping("/rest/karyawan/info")
+    @ResponseBody
+    public Karyawan infoKaryawan(@RequestParam Integer nip){
+        return absenService.cariByNip(nip);
+    }
+}

cc6406ddf89b665d09fb774458d97f6c54d52ae4
Endy Muhardin <endy.muhardin@gmail.com>
2014-03-24 14:07:12 +0700

    implement save record

--- aplikasi-absen-rest/src/main/java/com/muhardin/endy/training/ws/aplikasi/absen/rest/AbsenRestController.java
+++ aplikasi-absen-rest/src/main/java/com/muhardin/endy/training/ws/aplikasi/absen/rest/AbsenRestController.java
@@ -3,12 +3,17 @@ package com.muhardin.endy.training.ws.aplikasi.absen.rest;
 import com.muhardin.endy.training.ws.aplikasi.absen.Karyawan;
 import com.muhardin.endy.training.ws.aplikasi.absen.service.AbsenService;
 import java.util.List;
+import javax.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
 
 @Controller
 public class AbsenRestController {
@@ -21,7 +26,7 @@ public class AbsenRestController {
         return absenService.cariByNip(nip);
     }
     
-    @RequestMapping("/rest/karyawan/")
+    @RequestMapping(value="/rest/karyawan/", method = RequestMethod.GET)
     @ResponseBody
     public List<Karyawan> daftarKaryawan(
             @RequestParam(required = false)Integer start,
@@ -37,5 +42,11 @@ public class AbsenRestController {
         }
         
         return absenService.semuaKaryawan(start, rows);
+    }
+    
+    @RequestMapping(value="/rest/karyawan/", method = RequestMethod.POST)
+    @ResponseStatus(HttpStatus.CREATED)
+    public void simpan(@RequestBody @Valid Karyawan k){
+        absenService.simpan(k);
     }
 }

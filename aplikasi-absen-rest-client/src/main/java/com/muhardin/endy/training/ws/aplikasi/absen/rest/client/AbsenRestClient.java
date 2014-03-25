345e7a499a57a112b9b5ac522b36bd8c0460059a
Endy Muhardin <endy.muhardin@gmail.com>
2014-03-25 15:15:21 +0700

    implement basic auth untuk protokol REST

--- aplikasi-absen-rest-client/src/main/java/com/muhardin/endy/training/ws/aplikasi/absen/rest/client/AbsenRestClient.java
+++ aplikasi-absen-rest-client/src/main/java/com/muhardin/endy/training/ws/aplikasi/absen/rest/client/AbsenRestClient.java
@@ -3,16 +3,22 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package com.muhardin.endy.training.ws.aplikasi.absen.rest.client;
 
 import com.muhardin.endy.training.ws.aplikasi.absen.Karyawan;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import org.apache.http.auth.AuthScope;
+import org.apache.http.auth.UsernamePasswordCredentials;
+import org.apache.http.client.CredentialsProvider;
+import org.apache.http.client.HttpClient;
+import org.apache.http.impl.client.BasicCredentialsProvider;
+import org.apache.http.impl.client.HttpClientBuilder;
 import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.web.client.RestTemplate;
 
 /**
@@ -20,50 +26,58 @@ import org.springframework.web.client.RestTemplate;
  * @author endy
  */
 public class AbsenRestClient {
+
     private static final String SERVER_URL = "http://localhost:8080/aplikasi-absen-rest/api/rest";
-    private final RestTemplate restTemplate = new RestTemplate();
+    private final RestTemplate restTemplate;
 
     public AbsenRestClient() {
+        CredentialsProvider provider = new BasicCredentialsProvider();
+        UsernamePasswordCredentials credentials = new UsernamePasswordCredentials("endy", "123");
+        provider.setCredentials(AuthScope.ANY, credentials);
+        HttpClient client = HttpClientBuilder.create().setDefaultCredentialsProvider(provider).build();
+
+        restTemplate = new RestTemplate(new HttpComponentsClientHttpRequestFactory(client));
         restTemplate.setErrorHandler(new AbsenRestClientErrorHandler());
     }
-    
-    public Karyawan cariKaryawanByNip(Integer nip){
-        String url = SERVER_URL+"/karyawan/"+nip;
+
+    public Karyawan cariKaryawanByNip(Integer nip) {
+        String url = SERVER_URL + "/karyawan/" + nip;
         Karyawan k = restTemplate
                 .getForObject(url, Karyawan.class, new HashMap<Object, Object>());
         return k;
     }
-    
-    public List<Karyawan> semuaKaryawan(){
-        ParameterizedTypeReference<List<Karyawan>> typeRef 
-                = new ParameterizedTypeReference<List<Karyawan>>() {};
-        
+
+    public List<Karyawan> semuaKaryawan() {
+        ParameterizedTypeReference<List<Karyawan>> typeRef
+                = new ParameterizedTypeReference<List<Karyawan>>() {
+                };
+
         ResponseEntity<List<Karyawan>> response = restTemplate
-         .exchange(SERVER_URL+"/karyawan/", 
-                 HttpMethod.GET, null, 
-                 typeRef);
-        
-        System.out.println("Response Status : "+response.getStatusCode());
-        
+                .exchange(SERVER_URL + "/karyawan/",
+                        HttpMethod.GET, null,
+                        typeRef);
+
+        System.out.println("Response Status : " + response.getStatusCode());
+
         return response.getBody();
-    }    
+    }
 
     public void simpan(Karyawan k) {
         String url = SERVER_URL + "/karyawan/";
         restTemplate.postForObject(url, k, Object.class);
     }
-    
-    public void update(Karyawan k){
+
+    public void update(Karyawan k) {
         String url = SERVER_URL + "/karyawan/{nip}";
         restTemplate.put(url, k, k.getNip());
     }
-    
-    public void delete(Karyawan k){
+
+    public void delete(Karyawan k) {
         String url = SERVER_URL + "/karyawan/{nip}";
-        
+
         Map<String, Object> pathVariable = new HashMap<>();
         pathVariable.put("nip", k.getNip());
-        
+
         restTemplate.delete(url, pathVariable);
     }
 }

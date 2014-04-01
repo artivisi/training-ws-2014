e987f2b30eb828c1bbe86f2640780c73511ea0e2
Endy Muhardin <endy.muhardin@gmail.com>
2014-04-01 15:40:08 +0700

    handle upload part 1 : memproses header

--- aplikasi-storage-server/src/main/java/com/artivisi/training/ws/storage/controller/UploadController.java
+++ aplikasi-storage-server/src/main/java/com/artivisi/training/ws/storage/controller/UploadController.java
@@ -8,9 +8,12 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class UploadController {
@@ -40,5 +43,44 @@ public class UploadController {
         ResponseEntity<String> response = new ResponseEntity<String>(null, responseHeaders, HttpStatus.OK);
         return response;
 
+    }
+    
+    
+    @RequestMapping(value = "/upload/", method = RequestMethod.PUT)
+    public ResponseEntity<String> upload(@RequestParam("upload_id") String id, 
+            @RequestHeader("Content-Length") Long contentLength, 
+            @RequestHeader("Content-Range") String contentRange
+            ){
+        HttpHeaders responseHeaders = new HttpHeaders();
+        responseHeaders.set("Range", "0-1000");
+
+        System.out.println("Content-Length : "+contentLength);
+        System.out.println("Content-Range : "+contentRange);
+        
+        // parsing content range
+        String[] partTotal = contentRange.split("/");
+        if(partTotal.length != 2){
+            ResponseEntity<String> response = new ResponseEntity<String>(null, 
+                responseHeaders, HttpStatus.BAD_REQUEST);
+            return response;
+        }
+        String[] startEnd = partTotal[0].split("-");
+        if(startEnd.length != 2){
+            ResponseEntity<String> response = new ResponseEntity<String>(null, 
+                responseHeaders, HttpStatus.BAD_REQUEST);
+            return response;
+        }
+        
+        Long start = Long.valueOf(startEnd[0]);
+        Long end = Long.valueOf(startEnd[1]);
+        
+        System.out.println("Client upload start at byte : "+start);
+        System.out.println("Client upload end at byte : "+end);
+        System.out.println("Client upload size : "+contentLength);
+        System.out.println("Client upload total size : "+partTotal[1]);
+        
+        ResponseEntity<String> response = new ResponseEntity<String>(null, 
+                responseHeaders, HttpStatus.RESUME_INCOMPLETE);
+        return response;
     }
 }
